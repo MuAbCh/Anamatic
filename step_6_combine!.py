098f7c9ca993b0e6842ff6e739904aef67dd64e7
MuAbCh <mabdullahc19@gmail.com>
2024-10-05 02:19:45 -0400

    edited the step 6 file. Am now implemeting the new functioins with new APIs for a new step 6

--- step_6_combine!.py
+++ step_6_combine!.py
@@ -5,6 +5,25 @@ import gc
 import json
 import datetime as dt
 
+from utils.file_related import open_json_file, load_from_file
+
+from google.cloud import texttospeech
+
+import whisperx
+import ffmpeg
+
+import subprocess
+from whisperx.utils import get_writer
+import re
+
+import time
+from groq import Groq
+from dotenv import load_dotenv
+
+import random
+
+import requests
+
 import openvino_genai as ov_genai
 from transformers import pipeline
 import scipy
@@ -38,76 +57,162 @@ import logging
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Step 1 code ###########################################################
+
+def generate_text_helper(prompt_system, prompt_user, max_new_tokens= 1000, self_model="llama-3.1-70b-versatile", self_temperature=0.00):
+    load_dotenv(".env")
+    Groq_api_key  = os.getenv("GROQ_APIKEY")
+    client = Groq(api_key = Groq_api_key,)
+    chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content":prompt_system
+                },
+                {
+                    "role": "user",
+                    "content": prompt_user,
+                }
+            ],
+            model=self_model, temperature=self_temperature,
+        )
+    # returns just the text
+    return chat_completion.choices[0].message.content
 
 def generate_text(prompt, max_new_tokens, model_dir="./Llama-3.2-1B_instruct_openvino"):
+    system_prompt = """
+    You are a helpful assistant that generates song prompts for sonu.ai """
+
+    user_prompt = f"""
+    Can you give me a comma separated description of the song Blinding Lights by The Weeknd in 200 characters, 
+    including spaces for a song prompt for sonu.ai? You can't use the artist name so describe the vocals too.
     """
-    Generates text using OpenVINO's LLMPipeline.
-
-    Parameters:
-    - prompt (str): The input prompt for the language model.
-    - max_new_tokens (int): The maximum number of tokens to generate.
-    - model_dir (str): Directory where the model is stored.
-
-    Returns:
-    - str: Generated text.
-    """
-    pipe = ov_genai.LLMPipeline(model_dir, "CPU")
-    text = pipe.generate(prompt, max_new_tokens=max_new_tokens)
-    del pipe
-    gc.collect()
-    return text
+    script = generate_text_helper(system_prompt, user_prompt)
+    print(script)
+    return script
 
 
-def generate_music_description(script_text, generate_text_fn, temp_dir):
-    """
-    Generates a description for the background music based on the script.
+# Step 2 code ###########################################################
 
-    Parameters:
-    - script_text (str): The generated script text.
-    - generate_text_fn (function): Function to generate text.
-    - temp_dir (str): Path to the temporary directory.
+# Replace your Vercel domain
+base_url = 'https://suno-api-eight-eta.vercel.app/'
 
-    Returns:
-    - str: Description of the background music.
-    """
-    music_prompt = """
-    The above is a script for a TikTok video.
-    Please generate a short one-sentence description of the music that should be playing in the background of the video.
-    Include genre and mood.
-    Example:
-    "A short upbeat EDM tune with a catchy melody"
+def custom_generate_audio(payload):
+    url = f"{base_url}/api/custom_generate"
+    response = requests.post(url, json=payload, headers={'Content-Type': 'application/json'})
+    return response.json()
 
-    Music Description:
-    """
-    combined_prompt = script_text + '\n\n' + music_prompt
-    music_description = generate_text_fn(combined_prompt, 100)
-    return music_description.strip()
+def extend_audio(payload):
+    url = f"{base_url}/api/extend_audio"
+    response = requests.post(url, json=payload, headers={'Content-Type': 'application/json'})
+    return response.json()
 
+def generate_audio_by_prompt(payload):
+    url = f"{base_url}/api/generate"
+    response = requests.post(url, json=payload, headers={'Content-Type': 'application/json'})
+    return response.json()
 
-def generate_music(music_description, temp_dir):
-    """
-    Generates background music using the MusicGen model.
+def get_audio_information(audio_ids):
+    url = f"{base_url}/api/get?ids={audio_ids}"
+    response = requests.get(url)
+    return response.json()
 
-    Parameters:
-    - music_description (str): Description of the music.
-    - temp_dir (str): Path to the temporary directory.
+def get_quota_information():
+    url = f"{base_url}/api/get_limit"
+    response = requests.get(url)
+    return response.json()
 
-    Returns:
-    - str: Path to the generated music file.
-    """
-    synthesiser = pipeline("text-to-audio", "facebook/musicgen-small")
-    music = synthesiser(
-        music_description,
-        forward_params={"do_sample": True}
-    )
+def get_clip(clip_id):
+    url = f"{base_url}/api/clip?id={clip_id}"
+    response = requests.get(url)
+    return response.json()
+
+def generate_whole_song(clip_id):
+    payloyd = {"clip_id": clip_id}
+    url = f"{base_url}/api/concat"
+    response = requests.post(url, json=payload)
+    return response.json()
+
+# Function to save audio file locally in a specific directory
+def download_audio_file(url, file_name, output_dir):
     # Ensure the output directory exists
-    output_music_path = os.path.join(temp_dir, "background_music.wav")
-    scipy.io.wavfile.write(output_music_path, rate=music["sampling_rate"], data=music["audio"])
-    del synthesiser, music
-    gc.collect()
-    return output_music_path
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    # Full file path
+    file_path = os.path.join(output_dir, file_name)
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+        print(f"Downloaded: {file_path}")
+    else:
+        print(f"Failed to download {file_name}")
+    return file_path
+
+# Function to generate and download music files to a specific directory
+def generate_music_file(prompt, make_instrumental=False, wait_audio=False, retries=60, retry_delay=5, output_dir="./audio_files"):
+    """
+    Generates music based on a given text prompt and downloads the resulting audio files.
+
+    Parameters:
+    -----------
+    prompt : str
+        The text prompt used to generate the audio. This should describe the style, mood, and content of the music.
+        
+    make_instrumental : bool, optional (default=False)
+        If True, the generated audio will be instrumental without vocals. If False, it may include vocals.
+
+    wait_audio : bool, optional (default=False)
+        If True, the function will wait for the audio generation to complete before returning a response. If False, it will return as soon as the request is initiated.
+
+    retries : int, optional (default=60)
+        The maximum number of times to poll the server to check if the audio files are ready for download. Each retry is followed by a delay specified by `retry_delay`.
+
+    retry_delay : int, optional (default=5)
+        The number of seconds to wait between each retry while polling for the audio files' status.
+
+    output_dir : str, optional (default="./audio_files")
+        The directory where the audio files will be downloaded and stored. If the directory does not exist, it will be created.
+
+    Returns:
+    --------
+    tuple of str
+        Returns a tuple containing the paths of the downloaded audio files (audio_1.mp3, audio_2.mp3).
+        If audio generation fails or times out, it returns None.
+    """
+    
+    # Generate audio based on the provided prompt
+    data = generate_audio_by_prompt({
+        "prompt": prompt,
+        "make_instrumental": make_instrumental,
+        "wait_audio": wait_audio
+    })
+    
+    # Combine the IDs of generated audio
+    ids = f"{data[0]['id']},{data[1]['id']}"
+    
+    # Poll to check the status of the generated audio files
+    for _ in range(retries):
+        audio_data = get_audio_information(ids)
+        if audio_data[0]["status"] == 'streaming':
+            audio_url_1 = audio_data[0]['audio_url']
+            audio_url_2 = audio_data[1]['audio_url']
+            
+            # Download the audio files to the specified directory
+            audio_file_1 = download_audio_file(audio_url_1, 'audio_1.mp3', output_dir)
+            audio_file_2 = download_audio_file(audio_url_2, 'audio_2.mp3', output_dir)
+            return audio_file_1, audio_file_2
+        
+        # Wait for the specified delay before checking again
+        time.sleep(retry_delay)
+    
+    # Return None if the audio files could not be retrieved
+    return None
 
 
+# Step 3 Audio Generation ###########################################################
 # Original Working `synthesize_text_to_audio` Function
 def synthesize_text_to_audio(
     text: str,
