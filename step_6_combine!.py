17ad493a0c681c56f98894a562ef21877f797b62
MuAbCh <mabdullahc19@gmail.com>
2024-10-05 01:57:10 -0400

    changed some file permissions

--- step_6_combine!.py
+++ step_6_combine!.py
@@ -684,7 +684,7 @@ from moviepy.config import change_settings
 
 change_settings({"IMAGEMAGICK_BINARY": "/usr/bin/convert"})
 
- Step 9: Assemble the Video
+# Step 9: Assemble the Video
 temp_dir = "temp"
 voice_over_path = os.path.join(temp_dir, "voice_over.wav")
 music_path = os.path.join(temp_dir, "background_music.wav")
